43b817a1a89e81f9a9139e25ae008b118e23bc1e
DiegoMMF <diegomaldinifreyre@gmail.com>
2023-05-13 03:09:56 -0300

    clase 11

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+// * TIPOS PRIMITIVOS
+
+import { EnumType, TupleType } from "typescript";
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: void;
+let e: never;
+let f: null;
+let g: undefined;
+let h: any;
+let i: TupleType;
+let j: Object;
+let k: Array<any>;
+let l: EnumType;
+
 let phone: number;          // number explicito
 phone = 54234567;
 phone = 'hola';
@@ -43,3 +60,54 @@ console.log('subcadena', subcadena);
 let hex: number = 0xf00d;
 let binary: number = 0b1010;
 let octal: number = 0o744;
+
+// * Void
+
+// Tipo explicito con any
+function showInfo(user: any): any {
+    console.log('User Info', user.id, user.username, user.firstName);
+    // return 'hola';
+}
+
+showInfo({id: 1, username: 'luixaviles', firstName: 'Luis'});
+
+// Tipo Inferido
+function showFormattedInfo(user: any) {
+    console.log('User Info', `
+        id: ${user.id}
+        username: ${user.username}
+        firstName: ${user.firstName}
+    `);
+}
+
+showFormattedInfo({id: 1, username: 'luixaviles', firstName: 'Luis'});
+
+// tipo void, como tipo de dato en variable
+let unusable: void;
+unusable = null;        // error si tscondig.json { ..., strict: true, ... }
+unusable = undefined;
+
+// Tipo: Never
+
+function handleError(code: number, message: string): never{
+
+    // Process your code here
+    // Generate a message
+    throw new Error(`${message}. Code: ${code}`);
+}
+
+try {
+    handleError(404, 'Not Found');
+} catch (error) {
+}
+
+function sumNumbers(limit: number): never {
+    let sum = 0;
+    while(true) {
+        sum++;
+    }
+    // return sum;
+}
+
+sumNumbers(10);
+// ciclo infinito, programa nunca termina
